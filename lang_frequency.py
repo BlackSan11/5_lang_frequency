984180033338f37f6b07b0a4ad0dce4fc7e55f7d
Bogdan <bhelpdig@gmail.com>
2017-10-19 12:59:54 +0300

    в

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,7 +1,9 @@
 
 
-def load_data(filepath):
-    pass
+def load_data_from_file(filepath):
+    with open(filepath, "r", encoding="utf-8") as file_with_json:
+        json_from_file = json.load(file_with_json)
+        return json_from_file['features']
 
 
 def get_most_frequent_words(text):
